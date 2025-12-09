5ed2723127d16aa089916271aeb6de8c29bf7558
kimyt990501 <kimyt990501@naver.com>
2025-12-09 14:49:05 +0900

    refactor : 우천취소 예측 버튼 관련 수정

--- frontend/src/components/WeatherForm.vue
+++ frontend/src/components/WeatherForm.vue
@@ -55,9 +55,10 @@
     >
       <LoadingSpinner v-if="predictionLoading" size="sm" />
       <svg v-else width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-        <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"/>
+        <path d="M9 11l3 3L22 4"/>
+        <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"/>
       </svg>
-      {{ predictionLoading ? '분석 중...' : 'AI 우천취소 예측' }}
+      {{ predictionLoading ? 'AI가 분석 중...' : '경기 가능 여부 확인' }}
     </button>
   </div>
 </template>
